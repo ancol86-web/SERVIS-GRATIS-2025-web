da681e7f4fc9656b1839fe322e71cf3370e37cba
ancol86-web <ancolputra13@gmail.com>
2025-06-15 01:47:49 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Halaman Service Gratis BSO Bengkel HMDM ITS 2025 berhasil dimuat.");
+  // Tambahkan fungsi JavaScript jika dibutuhkan di sini
+});
